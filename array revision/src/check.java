15dff480b7ff08deb118f6b4d43d325b4aa27b76
unknown <vivekumar175@gmail.com>
2022-03-28 11:34:08 +0530

    dd

--- array revision/src/check.java
+++ array revision/src/check.java
@@ -1,9 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class check {
 
+    static int count = 0;
+    // public static int size;
+
     public static void main(String[] args) {
-        int arr[] = { 12, 10, 14, 85, 63, 81, 13 };
-        int a = arr[arr[4]];
-        System.out.println(a);
+        List list = new ArrayList<>();
+        list.add("abc");
+        list.add(200);
+        System.out.print(list.get(0) instanceof Object);
+        System.out.print(list.get(1) instanceof Integer);
+        int g1 = 1, g2 = 2;
+        System.out.println((g2 & g1));
+        if (count < 3) {
+            count++;
+            main(null);
+        } else {
+            return;
+        }
+        System.out.print(count);
     }
 
 }
